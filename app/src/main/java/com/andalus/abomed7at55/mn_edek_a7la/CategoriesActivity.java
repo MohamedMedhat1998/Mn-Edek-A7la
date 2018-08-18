fdee040b3d7e88a12a6b8facd224df64d79a69df
Abo_Med7at <abomed7at55@gmail.com>
2018-08-18 11:09:48 +0200

    Added search bar

--- app/src/main/java/com/andalus/abomed7at55/mn_edek_a7la/CategoriesActivity.java
+++ app/src/main/java/com/andalus/abomed7at55/mn_edek_a7la/CategoriesActivity.java
@@ -32,10 +32,11 @@ import butterknife.ButterKnife;
 public class CategoriesActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener, OnCategoryClickListener {
 
-    private static final int SPAN_COUNT = 2;
-
     @BindView(R.id.rv_categories)
     RecyclerView rvCategories;
+
+    @BindView(R.id.rv_search_result)
+    RecyclerView rvSearchResult;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -67,28 +68,6 @@ public class CategoriesActivity extends AppCompatActivity
         } else {
             super.onBackPressed();
         }
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.categories, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 
     @SuppressWarnings("StatementWithEmptyBody")
