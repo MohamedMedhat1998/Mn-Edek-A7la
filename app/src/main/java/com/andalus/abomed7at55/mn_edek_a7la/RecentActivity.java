5ca83cc285d4912e79d9237cadbcd8f35b3f05db
Abo_Med7at <abomed7at55@gmail.com>
2018-08-28 16:58:53 +0200

    Added Firebase remote config

--- app/src/main/java/com/andalus/abomed7at55/mn_edek_a7la/RecentActivity.java
+++ app/src/main/java/com/andalus/abomed7at55/mn_edek_a7la/RecentActivity.java
@@ -1,13 +1,55 @@
 package com.andalus.abomed7at55.mn_edek_a7la;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.remoteconfig.FirebaseRemoteConfig;
+import com.google.firebase.remoteconfig.FirebaseRemoteConfigSettings;
 
 public class RecentActivity extends AppCompatActivity {
+
+    private FirebaseRemoteConfig mFirebaseRemoteConfig = FirebaseRemoteConfig.getInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recent);
+
+        FirebaseRemoteConfigSettings configSettings = new FirebaseRemoteConfigSettings.Builder()
+                .setDeveloperModeEnabled(BuildConfig.DEBUG)
+                .build();
+
+        mFirebaseRemoteConfig.setConfigSettings(configSettings);
+
+        mFirebaseRemoteConfig.setDefaults(R.xml.remote_config_defaults);
+
+        mFirebaseRemoteConfig.fetch(0)
+                .addOnCompleteListener(this, new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if (task.isSuccessful()) {
+                            Toast.makeText(RecentActivity.this, "Fetch Succeeded",
+                                    Toast.LENGTH_SHORT).show();
+                            // After config data is successfully fetched, it must be activated before newly fetched
+                            // values are returned.
+                            mFirebaseRemoteConfig.activateFetched();
+                        } else {
+                            Toast.makeText(RecentActivity.this, "Fetch Failed",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                        //TODO do something when the data is returned
+                        doWhenReceived();
+                    }
+                });
+    }
+
+    private void doWhenReceived(){
+        String s = mFirebaseRemoteConfig.getString("test_key");
+        Log.d("REMOTE CONFIG VALUE",s);
     }
 }
